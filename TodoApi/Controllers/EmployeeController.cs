1bcaa8dc2863412677eec9f56568ee62fd1e1db8
TylerTCR <tcil112000@gmail.com>
2022-01-11 14:58:09 -0500

    Week 14 Endpoint

--- TodoApi/Controllers/EmployeeController.cs
+++ TodoApi/Controllers/EmployeeController.cs
@@ -21,12 +21,16 @@ namespace TodoApi.Controllers
         
         [Produces("application/json")]
         [HttpGet("{email}")]
-        public async Task<IActionResult> VerifyEmployee(string _email)
+        public async Task<IActionResult> VerifyEmployee(string email)
         {
             try
             {
-                var employee = _context.Employees.Where(b => b.Email == _email).FirstOrDefault();
-                return Ok(employee.Email);
+                var employee = _context.Employees.Where(b => b.Email == email).FirstOrDefault();
+                if (employee.Email == email) {
+                    return Ok(employee.Email);
+                } else {
+                    return Ok("NotFound");
+                }
             }
             catch
             {
--- TodoApi/Models/tylercalderonContext.cs
+++ TodoApi/Models/tylercalderonContext.cs
@@ -36,7 +36,7 @@ namespace TodoApi.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseMySql("server=codeboxx.cq6zrczewpu2.us-east-1.rds.amazonaws.com;database=tylercalderon;user=codeboxx;password=Codeboxx1!;treattinyasboolean=true", Microsoft.EntityFrameworkCore.ServerVersion.Parse("5.7.33-mysql"));
             }
         }
